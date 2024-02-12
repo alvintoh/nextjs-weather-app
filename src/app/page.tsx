956f4fccb28d374a24c3e36e4f969b165c1dbadb
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-02-12 12:05:02 +0800

    [feature/forecast-weather-details] Implement feature for display current day forecast and weather details with icons displayed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import WeatherDetails from "@/components/WeatherDetails";
+import WeatherIcon from "@/components/WeatherIcon";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { format, parseISO } from "date-fns";
@@ -87,19 +92,80 @@ export default function Home() {
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         {/* today data */}
-        <section>
-          <div>
+        <section className="space-y-4">
+          <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
               <p className="text-lg">
                 ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
               </p>
             </h2>
-            <div></div>
+            <Container className=" gap-10 px-6 items-center">
+              {/* temperature */}
+              <div className=" flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 299.05)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}°↓{" "}
+                  </span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°↑
+                  </span>
+                </p>
+              </div>
+              {/* time and weather icon */}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(item.dt_txt), "HH:mm a")}
+                    </p>
+                    <WeatherIcon
+                      iconName={getDayOrNightIcon(
+                        item.weather[0].icon,
+                        item.dt_txt
+                      )}
+                    />
+                    <p>{convertKelvinToCelsius(item?.main.temp ?? 0)} °</p>
+                  </div>
+                ))}
+              </div>
+            </Container>
+          </div>
+          <div className="flex gap-4">
+            {/* left */}
+            <Container className="w-fit justify-center flex-col px-4 items-center">
+              <p className="capitalize text-center">
+                {firstData?.weather[0].description}
+              </p>
+              <WeatherIcon
+                iconName={getDayOrNightIcon(
+                  firstData?.weather[0].icon ?? "",
+                  firstData?.dt_txt ?? ""
+                )}
+              />
+            </Container>
+            <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
+              <WeatherDetails></WeatherDetails>
+            </Container>
+            {/* right */}
           </div>
         </section>
         {/* 7 days forecast data */}
-        <section></section>
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forecast (7 days)</p>
+        </section>
       </main>
     </div>
   );
---
+++ src/utils/getDayOrNightIcon.ts
@@ -0,0 +1,8 @@
+export function getDayOrNightIcon(
+  iconName: string,
+  dateTimeString: string
+): string {
+  const hours = new Date(dateTimeString).getHours(); // Get hours from the given date and time string
+  const isDayTime = hours >= 6 && hours < 18; // Consider daytime from 6 AM to 6 PM
+  return isDayTime ? iconName.replace(/.$/, "d") : iconName.replace(/.$/, "n"); // Replace last character with 'd' or 'n' based on day or night
+}
