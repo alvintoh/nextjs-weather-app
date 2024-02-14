e02e8ed7574a9314b24c8c35b8d127173feee934
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-02-15 01:00:31 +0800

    [feature/navbar] Create suggestions list, and set current location functionality and able to refresh website loading with weather skeleton. Set mobile responsive view under page.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,21 @@ import { MdOutlineMyLocation } from "react-icons/md";
 import { MdOutlineLocationOn } from "react-icons/md";
 import SearchBox from "./SearchBox";
 import axios from "axios";
+import { loadingCityAtom, placeAtom } from "@/app/atom";
+import { useAtom } from "jotai";
 
-type Props = {};
+type Props = { location?: string };
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
-export default function Navbar({}: Props) {
+export default function Navbar({ location }: Props) {
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
   //
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [, setPlace] = useAtom(placeAtom);
+  const [_, setLoadingCity] = useAtom(loadingCityAtom);
 
   async function handleInputChange(value: string) {
     setCity(value);
@@ -45,36 +49,101 @@ export default function Navbar({}: Props) {
     setShowSuggestions(false);
   }
 
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    setLoadingCity(true);
+    // Prevent our page from refreshing when the form is submitted
+    e.preventDefault();
+    if (suggestions.length === 0) {
+      setError("Location not found");
+      setLoadingCity(false);
+    } else {
+      setError("");
+      setTimeout(() => {
+        setLoadingCity(false);
+        setPlace(city);
+        setShowSuggestions(false);
+      }, 500);
+    }
+  }
+
+  function handleCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          });
+        } catch (error) {
+          setLoadingCity(false);
+        }
+      });
+    } else {
+      setError("Geolocation is not supported by this browser.");
+      setLoadingCity(false);
+    }
+  }
+
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-        <p className="flex items-center justify-center gap-2">
-          <h2 className="text-gray-500 text-3xl">Weather</h2>
-          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
-        </p>
-        {/* */}
-        <section className="flex gap-2 items-center">
-          <MdOutlineMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
-          <MdOutlineLocationOn className="text-3xl" />
-          <p className="text-slate-900/80 text-sm">Singapore</p>
-          <div>
-            <SearchBox
-              value={city}
-              //onSubmit={}
-              onChange={(e) => handleInputChange(e.target.value)}
+    <>
+      <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+          <p className="flex items-center justify-center gap-2">
+            <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
+          </p>
+          {/* */}
+          <section className="flex gap-2 items-center">
+            <MdOutlineMyLocation
+              title="Your Current Location"
+              onClick={handleCurrentLocation}
+              className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"
             />
-            <SuggestionBox
-              {...{
-                showSuggestions,
-                suggestions,
-                handleSuggestionClick,
-                error,
-              }}
-            />
-          </div>
-        </section>
-      </div>
-    </nav>
+            <MdOutlineLocationOn className="text-3xl" />
+            <p className="text-slate-900/80 text-sm"> {location} </p>
+            <div className="relative hidden md:flex">
+              {/* SearchBox */}
+              <SearchBox
+                value={city}
+                onSubmit={handleSubmitSearch}
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+              <SuggestionBox
+                {...{
+                  showSuggestions,
+                  suggestions,
+                  handleSuggestionClick,
+                  error,
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className="flex max-w-7xl px-3 md:hidden">
+        {/* Mobile view SearchBox as now is not hidden on md screen*/}
+        <div className="relative">
+          <SearchBox
+            value={city}
+            onSubmit={handleSubmitSearch}
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+          <SuggestionBox
+            {...{
+              showSuggestions,
+              suggestions,
+              handleSuggestionClick,
+              error,
+            }}
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
@@ -91,15 +160,22 @@ function SuggestionBox({
 }) {
   return (
     <>
-      {(showSuggestions && suggestions.length > 1) ||
-        (error && (
-          <ul className="mb-4 bg-white absolute top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
-            {error && suggestions.length < 1 && (
-              <li className="text-red-500 p-1">{error}</li>
-            )}
-            <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
-          </ul>
-        ))}
+      {((showSuggestions && suggestions.length > 1) || error) && (
+        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+          {error && suggestions.length < 1 && (
+            <li className="text-red-500 p-1 "> {error}</li>
+          )}
+          {suggestions.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => handleSuggestionClick(item)}
+              className="cursor-pointer p-1 rounded hover:bg-gray-200"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
